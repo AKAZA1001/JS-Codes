c5183525a6fb541e534fb3fb08413d4e1f7a3d8c
AKAZA1001 <136004875+AKAZA1001@users.noreply.github.com>
2023-06-09 11:15:34 +0530

    Adding Second.js

---
+++ Second.js
@@ -0,0 +1,16 @@
+// Stock sell and buy
+
+function maxProfit(prices) {
+  let minPrice = Infinity;
+  let maxProfit = 0;
+  for (let i = 0; i < prices.length; i++) {
+    if (prices[i] < minPrice) {
+      minPrice = prices[i];
+    } else if (prices[i] - minPrice > maxProfit) {
+      maxProfit = prices[i] - minPrice;
+    }
+  }
+  return maxProfit;
+}
+const prices = [7, 4, 5, 3, 4, 6];
+console.log(maxProfit(prices));
